11d029960ed1142bc8a789776186da31333ad86a
Mahbub Morshed <mahbub.bogra30@gmail.com>
2022-04-18 23:27:59 +0600

    blog

--- src/Pages/blog/Blog.js
+++ src/Pages/blog/Blog.js
@@ -6,15 +6,29 @@ const Blog = () => {
     <div className="container w-75">
       <Accordion defaultActiveKey="0" flush>
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Header>
+            Difference between authorization and authentication
+          </Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            Simply put, authentication is the process of verifying who someone
+            is, whereas authorization is the process of verifying what specific
+            applications, files, and data a user has access to. The situation is
+            like that of an airline that needs to determine which people can
+            come on board
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Header>
+            Why are you using firebase? What other options do you have to
+            implement authentication?
+          </Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            You can use Firebase Authentication to allow users to sign in to
+            your app using one or more sign-in methods, including email address
+            and password sign-in, and federated identity providers such as
+            Google Sign-in and Facebook Login. This tutorial gets you started
+            with Firebase Authentication by showing you how to add email address
+            and password sign-in to your app. Add and initial
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
